2a552275ff520668cb5699347df74cb01b4fc536
Alireza SadraiiRad <alireza.sadraiirad@miba.com>
2022-08-14 23:57:57 +0200

    resolve steps bidirectional problems in dots

--- src/components/steps/steps.tsx
+++ src/components/steps/steps.tsx
@@ -23,15 +23,15 @@ const Steps: React.VoidFunctionComponent<StepsProps> = ({
           total,
         })}
       </div>
-      <div className="ml-2 flex items-center">
+      <div className="flex items-center ms-2">
         {[...Array(total)].map((_, i) => {
           return (
             <span
               key={i}
-              className={clsx("ml-3 h-2  w-2 rounded-full transition-all", {
+              className={clsx("h-2 w-2  rounded-full transition-all ms-3", {
                 "bg-primary-400": i <= current,
                 "bg-gray-300": i > current,
-                "ring-primary-200 animate-pulse ring-4": i === current,
+                "animate-pulse ring-4 ring-primary-200": i === current,
               })}
             />
           );
