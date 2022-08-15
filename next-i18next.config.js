c5b083830023ae9bc7e5ea34ec1be36a348dedbb
Alireza SadraiiRad <alireza.sadraiirad@miba.com>
2022-08-15 16:21:15 +0200

    change default locale

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   i18n: {
-    defaultLocale: "en",
+    defaultLocale: "fa",
     locales: ["en", "es", "de", "fr", "it", "sv", "pt-BR", "fa"],
     localePath: path.resolve("./public/locales"),
     reloadOnPrerender: process.env.NODE_ENV === "development",
